b25eaa82201a18ba75e2d863fc9a9a0146256459
Scottjrw <wjrpower@gmail.com>
2021-04-10 10:27:53 +0800

    add address bar + initial compile

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,102 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
 import './App.css';
+import logo from '../logo.svg'
 import CharityBazaar from '../abis/CharityBazaar'
+import Addressbar from './Addressbar'
 import Main from './Main'
 
-function App() {
+class App extends Component{
   
+  async componentDidMount(){
+    await this.getWeb3Provider();
+    await this.connectToBlockchain();
+  }
+
+  async getWeb3Provider(){
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+        window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+  async connectToBlockchain(){
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    this.setState({account: accounts[0]})
+    const networkId = await web3.eth.net.getId()
+    const networkData = CharityBazaar.networks[networkId];
+    console.log(networkData);
+    if(networkData) {
+      const deployedCharityBazaar = new web3.eth.Contract(CharityBazaar.abi, networkData.address);
+      this.setState({deployedCharityBazaar: deployedCharityBazaar});
+      // get the number of total items on the blockchain; -1 because idCounter start from 1
+      // const totalNumber = await deployedCharityBazaar.methods.idCounter().call() -1 ;
+      // console.log(totalNumber);
+      // this.setState({totalNumber})
+      // // load items
+      // for (var i = 1;i<= totalNumber;i++) {
+      //   const item = await deployedCharityBazaar.methods.itemList(i).call();
+      //   this.setState({
+      //     items:[...this.state.items, item]
+      //   });
+      // }
+      // this.setState({loading: false})
+      // console.log(this.state.items)
+    } else {
+      window.alert('CharityBazaar contract is not found in your blockchain.')
+    }
+  }
+  
+  constructor(props){
+    super(props);
+    this.state = {
+        account: '', // account[0]
+        totalNumber: 0, // total number of items
+        items: [], // items on sale
+        loading: true
+    };
+
+        // TODO: modify this
+        // this.bidItem = async (itemName, itemPrice) => {
+        //     this.setState ({loading: true})
+        //     const gasAmount = await this.state.deployedEthbay.methods.createItem(itemName, itemPrice).estimateGas({from: this.state.account})
+        //     this.state.deployedEthbay.methods.createItem(itemName, itemPrice).send({from: this.state.account, gas: gasAmount})
+        //     .once('receipt', async (receipt)=> {
+        //       const totalNumber = await this.state.deployedEthbay.methods.totalNumber().call();
+        //       this.setState({totalNumber});
+        //       this.setState({items: []});
+        //       for (var i = 1;i<= totalNumber;i++) {
+        //         const item = await this.state.deployedEthbay.methods.items(i).call();
+        //         this.setState({
+        //           items:[...this.state.items, item]
+        //         });
+        //       }
+        //       this.setState({loading: false});
+        //     })
+        // }
+  }
+
+    render() {
+      return (
+        <div>
+          <Addressbar account={this.state.account}/>
+          <div className="container-fluid mt-5">
+            <div className="row">
+              <main>
+                <img src={logo} className="App-logo" alt="logo" />
+              </main>
+            </div>
+          </div>
+        </div>
+      );
+    }
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,40 +1,53 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class Main extends Component {
-  render() {
-    return (
-      <div id="content">
-        <h2>Add Item</h2>
-        <form onSubmit = 
-          {async (event) => {
-            event.preventDefault();
-            const itemName = this.itemName.value
-            const sellingPrice = window.web3.utils.toWei(this.sellingPrice.value.toString(), 'Ether')
-            await this.props.createItem(itemName, sellingPrice)}
-          }>
-        <div className="form-group mr-sm-2">
-            <input 
-            id="itemName"
-            type="text"
-            ref={(input)=>{this.itemName=input}}
-            className="form-control"
-            placeholder="Item Name"
-            required/>
-        </div>
-        <div>
-            <input 
-            id="sellingPrice"
-            type="text"
-            ref={(input)=>{this.sellingPrice=input}}
-            className="form-control"
-            placeholder="Selling Price"
-            required/>
-        </div>
-        <button type="submit" className="btn btn-primary">Add Item</button>
-        </form>
-      </div>
-    );
-  }
-}
+// class Main extends Component {
+//   render() {
+//     return (
+//       <div id="content">
+//         <h2>Item List</h2>
+//         <table className="table">
+//         <thead id="itemList">
+//           <tr>
+//             <th scope="col">#</th>
+//             <th scope="col">Name</th>
+//             <th scope="col">Price</th>
+//             <th scope="col">Owner</th>
+//             <th scope="col"></th>
+//           </tr> 
+//         </thead>
+//         <tbody id="itemList">
+//             {this.props.itemList.map((item, key)=>{
+//                 return(
+//                     <tr key={key}>
+//                     <th scope="row">{item.itemId.toString()}</th>   
+//                     <td>{item.itemName}</td> 
+//                     <td>{window.web3.utils.fromWei(item.rice.toString(), 'Ether')} ETH </td>
+//                     <td>{item.owner}</td>
+//                     <td>
+//                       {
+//                         !item.isItemSold
+//                           ?
+//                           <button 
+//                             id = {item.itemId}
+//                             value = {item.itemPrice}
+//                             onClick={async (event)=>{
+//                               await this.props.buyItem(event.target.id, event.target.value);
+//                             }}
+//                           >
+//                             Buy
+//                           </button>
+//                           : 
+//                           null
+//                         }
+//                     </td>
+//                   </tr>
+//                 )
+//             })}
+//         </tbody>
+//         </table>
+//       </div>
+//     );
+//   }
+// }
 
-export default Main;
+// export default Main;
